2b96d308935c084c62492dd79ad4fff13610a0aa
dhruv1008 <dhruvsharma10082004@gmail.com>
2023-09-06 16:50:02 +0530

    staggered view updated

--- lib/home.dart
+++ lib/home.dart
@@ -3,7 +3,12 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:noteapp/colors.dart';
 import 'package:noteapp/sidedrawer.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart'; // staggered view 
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart'; // staggered view
+
+String note1 =
+    "This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note This is bigger note";
+String note2 =
+    "This is smaller note yes this  This is bigger note This is bigger note This is bigger note This is bigger note";
 
 class Home extends StatelessWidget {
   GlobalKey<ScaffoldState> _drawerKey =
@@ -22,121 +27,251 @@ class Home extends StatelessWidget {
       backgroundColor: bgColor,
       body: SafeArea(
         child: Container(
-          child: Column(
-            children: [
-              Container(
-                  // search bar code
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                  width: MediaQuery.of(context)
-                      .size
-                      .width, // to get the width of the device
-                  height: 55,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: cardColor,
-                      boxShadow: [
-                        BoxShadow(
-                            color: black.withOpacity(0.2),
-                            spreadRadius: 1,
-                            blurRadius: 3)
-                      ]),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment
-                        .spaceBetween, // it is used when you have to put one object at start and other at the end
-                    children: [
-                      Row(
-                        children: [
-                          IconButton(
-                              onPressed: () {
-                                _drawerKey.currentState!
-                                    .openDrawer(); // function to open drawer from the button
-                              },
-                              icon: const Icon(
-                                Icons.menu,
-                                color: white,
-                              )),
-                          Container(
-                            height: 55,
-                            width: 205,
-                            decoration: const BoxDecoration(
-                                // border: Border.all(color: Colors.white)
-                                ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "Search Your Notes ",
-                                  style: TextStyle(
-                                      color: white.withOpacity(
-                                          0.5), // used to set the transparency of the color
-                                      fontSize: 16),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                      // const SizedBox(width: 30,),
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
+          child: SingleChildScrollView(
+            // making app scrollable
+            child: Column(
+              children: [
+                Container(
+                    // search bar code
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 10),
+                    width: MediaQuery.of(context)
+                        .size
+                        .width, // to get the width of the device
+                    height: 55,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: cardColor,
+                        boxShadow: [
+                          BoxShadow(
+                              color: black.withOpacity(0.2),
+                              spreadRadius: 1,
+                              blurRadius: 3)
+                        ]),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment
+                          .spaceBetween, // it is used when you have to put one object at start and other at the end
+                      children: [
+                        Row(
                           children: [
-                            TextButton(
-                                style: ButtonStyle(
-                                    //ButtonStyle is used to set the property of the button
-                                    // here we have given the button style when user taps on it
-                                    overlayColor: // sets the tap color
-                                        MaterialStateColor.resolveWith(
-                                            (states) => white.withOpacity(0.1)),
-                                    shape: MaterialStateProperty
-                                        .all< // sets the tap shape
-                                                RoundedRectangleBorder>(
-                                            RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(50.0),
-                                    ))),
-                                onPressed: () {},
-                                child: const Icon(
-                                  Icons.grid_view,
+                            IconButton(
+                                onPressed: () {
+                                  _drawerKey.currentState!
+                                      .openDrawer(); // function to open drawer from the button
+                                },
+                                icon: const Icon(
+                                  Icons.menu,
                                   color: white,
                                 )),
-                            const SizedBox(width: 10),
-                            const CircleAvatar(
-                              backgroundColor: white,
-                              radius:
-                                  16, // size define of circular avatar image
-                            )
+                            Container(
+                              height: 55,
+                              width: 205,
+                              decoration: const BoxDecoration(
+                                  // border: Border.all(color: Colors.white)
+                                  ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Search Your Notes ",
+                                    style: TextStyle(
+                                        color: white.withOpacity(
+                                            0.5), // used to set the transparency of the color
+                                        fontSize: 16),
+                                  ),
+                                ],
+                              ),
+                            ),
                           ],
                         ),
-                      )
+                        // const SizedBox(width: 30,),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              TextButton(
+                                  style: ButtonStyle(
+                                      //ButtonStyle is used to set the property of the button
+                                      // here we have given the button style when user taps on it
+                                      overlayColor: // sets the tap color
+                                          MaterialStateColor.resolveWith(
+                                              (states) =>
+                                                  white.withOpacity(0.1)),
+                                      shape: MaterialStateProperty
+                                          .all< // sets the tap shape
+                                                  RoundedRectangleBorder>(
+                                              RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(50.0),
+                                      ))),
+                                  onPressed: () {},
+                                  child: const Icon(
+                                    Icons.grid_view,
+                                    color: white,
+                                  )),
+                              const SizedBox(width: 10),
+                              const CircleAvatar(
+                                backgroundColor: white,
+                                radius:
+                                    16, // size define of circular avatar image
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    )),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  // trick to check size of container
+                  // decoration: BoxDecoration(
+                  //   border: Border.all(color: white)
+                  // ),
+                  margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "All",
+                        style: TextStyle(
+                            color: white.withOpacity(0.5),
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold),
+                      ),
                     ],
-                  )),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                // trick to check size of container
-                // decoration: BoxDecoration(
-                //   border: Border.all(color: white)
-                // ),
-                margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "All",
-                      style: TextStyle(
-                          color: white.withOpacity(0.5),
-                          fontSize: 13,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ],
+                  ),
                 ),
-              )
-            ],
+
+                nonhiglight_nte(),
+                highlighted_nte()
+                
+              ],
+            ),
           ),
         ),
       ),
     );
   }
+}
+
+
+Widget nonhiglight_nte(){
+   return Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 15,
+                    ),
+                    child: StaggeredGridView.countBuilder(
+                        // here main axis is vertical and the cross axis is horizontal like column
+                        physics:
+                            NeverScrollableScrollPhysics(), // its internal scrolling get closed
+                        shrinkWrap: true, // it is always neccessary
+                        itemCount: 10, // gives the number of count
+                        mainAxisSpacing:
+                            12, // gives space between item in main axis
+                        crossAxisSpacing:
+                            12, // gives space between item in  cross axis
+                        crossAxisCount: 4, // means 4 columns would be there
+                        staggeredTileBuilder: (index) => const StaggeredTile
+                                .fit(
+                            2), // this number of column a tile will get scatterred // vertivally it will takes space as per its sixe
+                        itemBuilder: (context, index) => InkWell(
+                              // same as of listview builder
+                              // widget ui of one particular tile
+                              onTap: () {
+                                // Navigator.push(
+                                //     context,
+                                //     MaterialPageRoute(
+                                //         builder: (context) => NoteView()));
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: white.withOpacity(0.4)),
+                                    borderRadius: BorderRadius.circular(7)),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text("HEADING",
+                                        style: TextStyle(
+                                            color: white,
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold)),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      index.isEven
+                                          ? note1.length > 250
+                                              ? "${note1.substring(0, 250)}......."
+                                              : note1
+                                          : note2,
+                                      style: const TextStyle(color: white),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            )));
+}
+
+Widget highlighted_nte() {
+  return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 15,
+      ),
+      child: StaggeredGridView.countBuilder(
+          // here main axis is vertical and the cross axis is horizontal like column
+          physics:
+              NeverScrollableScrollPhysics(), // its internal scrolling get closed
+          shrinkWrap: true, // it is always neccessary
+          itemCount: 10, // gives the number of count
+          mainAxisSpacing: 12, // gives space between item in main axis
+          crossAxisSpacing: 12, // gives space between item in  cross axis
+          crossAxisCount: 4, // means 4 columns would be there
+          staggeredTileBuilder: (index) => const StaggeredTile.fit(
+              2), // this number of column a tile will get scatterred // vertivally it will takes space as per its sixe
+          itemBuilder: (context, index) => InkWell(
+                // same as of listview builder
+                // widget ui of one particular tile
+                onTap: () {
+                  // Navigator.push(
+                  //     context,
+                  //     MaterialPageRoute(
+                  //         builder: (context) => NoteView()));
+                },
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color:
+                          index.isEven ? Colors.green[900] : Colors.blue[900],
+                      border: index.isEven
+                          ? Border.all(color: Colors.green.shade900)
+                          : Border.all(color: Colors.blue.shade900),
+                      borderRadius: BorderRadius.circular(7)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text("HEADING",
+                          style: TextStyle(
+                              color: white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold)),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        index.isEven
+                            ? note1.length > 250
+                                ? "${note1.substring(0, 250)}......."
+                                : note1
+                            : note2,
+                        style: const TextStyle(color: white),
+                      )
+                    ],
+                  ),
+                ),
+              )));
 }
--- lib/sidedrawer.dart
+++ lib/sidedrawer.dart
@@ -1,6 +1,7 @@
 // here we are creating the side drawer ui design
 
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:noteapp/colors.dart';
 
 class side_Drawer extends StatefulWidget {
@@ -16,7 +17,8 @@ class _side_DrawerState extends State<side_Drawer> {
     return
         // safe area brings the widget below the notification bar
         // now you can desgin the page as normal you make in flutter
-      Drawer(// it is must 
+        Drawer(
+      // it is must
       child: Container(
         decoration: const BoxDecoration(color: bgColor),
         child: SafeArea(
@@ -37,10 +39,18 @@ class _side_DrawerState extends State<side_Drawer> {
                 color: white.withOpacity(0.3),
               ),
               sectionOne(),
-              SizedBox(height: 8,),
+              const SizedBox(
+                height: 8,
+              ),
               sectiontwo(),
-              SizedBox(height: 8,),
-              sectionsetting()
+             const  SizedBox(
+                height: 8,
+              ),
+              sectionsetting(),
+             const  SizedBox(
+                height: 8,
+              ),
+              
               
             ],
           ),
@@ -51,110 +61,97 @@ class _side_DrawerState extends State<side_Drawer> {
   }
 }
 
-
-Widget sectionOne(){
+Widget sectionOne() {
   return Container(
-                margin: const EdgeInsets.only(
-                    right:
-                        10),
-                        // line number 40 and 44 have this  so that on tap color looks better
-                        // It uses MaterialStateProperty.all to specify that the background color should be the same for all button states.
-                        // MaterialStateProperty.all is a utility function in Flutter that's commonly used to set a single value for a property for all possible states of a widget. It's often used for defining properties of interactive widgets like buttons, where the appearance or behavior may change based on different states, such as when the button is pressed, hovered over, or in its default state.
-                child: TextButton(
-                    onPressed: () {},
-                    style: ButtonStyle( // used for styling the text button 
-                        backgroundColor: MaterialStateProperty.all( //
-                            Colors.orangeAccent.withOpacity(0.3)),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                              const  RoundedRectangleBorder(
-                              borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(50),
-                              bottomRight: Radius.circular(50)),
-                        ))),
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 5, horizontal: 5),
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.lightbulb,
-                            size: 27,
-                            color: Colors.yellow[400],
-                          ),
-                          const SizedBox(
-                            width: 27,
-                          ),
-                          Text(
-                            "Notes",
-                            style: TextStyle(
-                                color: white.withOpacity(0.7), fontSize: 18),
-                          )
-                        ],
-                      ),
-                    )),
-              );
+    margin: const EdgeInsets.only(right: 10),
+    // line number 40 and 44 have this  so that on tap color looks better
+    // It uses MaterialStateProperty.all to specify that the background color should be the same for all button states.
+    // MaterialStateProperty.all is a utility function in Flutter that's commonly used to set a single value for a property for all possible states of a widget. It's often used for defining properties of interactive widgets like buttons, where the appearance or behavior may change based on different states, such as when the button is pressed, hovered over, or in its default state.
+    child: TextButton(
+        onPressed: () {},
+        style: ButtonStyle(
+            // used for styling the text button
+            backgroundColor: MaterialStateProperty.all(//
+                Colors.orangeAccent.withOpacity(0.3)),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(50),
+                  bottomRight: Radius.circular(50)),
+            ))),
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+          child: Row(
+            children: [
+              Icon(
+                Icons.lightbulb,
+                size: 27,
+                color: Colors.yellow[400],
+              ),
+              const SizedBox(
+                width: 27,
+              ),
+              Text(
+                "Notes",
+                style: TextStyle(color: white.withOpacity(0.7), fontSize: 18),
+              )
+            ],
+          ),
+        )),
+  );
 }
 
-Widget sectiontwo(){
+Widget sectiontwo() {
   return Container(
-                margin: const EdgeInsets.only(
-                    right:
-                        10),
-                child: TextButton(
-                    onPressed: () {},
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 5, horizontal: 5),
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.archive_outlined,
-                            size: 27,
-                            color: Colors.blue[400],
-                          ),
-                          const SizedBox(
-                            width: 27,
-                          ),
-                          Text(
-                            "Archive ",
-                            style: TextStyle(
-                                color: white.withOpacity(0.7), fontSize: 18),
-                          )
-                        ],
-                      ),
-                    )),
-              );
+    margin: const EdgeInsets.only(right: 10),
+    child: TextButton(
+        onPressed: () {},
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+          child: Row(
+            children: [
+              Icon(
+                Icons.archive_outlined,
+                size: 27,
+                color: Colors.blue[400],
+              ),
+              const SizedBox(
+                width: 27,
+              ),
+              Text(
+                "Archive ",
+                style: TextStyle(color: white.withOpacity(0.7), fontSize: 18),
+              )
+            ],
+          ),
+        )),
+  );
 }
 
-
-Widget sectionsetting(){
+Widget sectionsetting() {
   return Container(
-                margin: const EdgeInsets.only(
-                    right:
-                        10),
-                child: TextButton(
-                    onPressed: () {},
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 5, horizontal: 5),
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.settings_outlined,
-                            size: 27,
-                            color: Colors.red[400],
-                          ),
-                          const SizedBox(
-                            width: 27,
-                          ),
-                          Text(
-                            "Settings ",
-                            style: TextStyle(
-                                color: white.withOpacity(0.7), fontSize: 18),
-                          )
-                        ],
-                      ),
-                    )),
-              );
+    margin: const EdgeInsets.only(right: 10),
+    child: TextButton(
+        onPressed: () {},
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+          child: Row(
+            children: [
+              Icon(
+                Icons.settings_outlined,
+                size: 27,
+                color: Colors.red[400],
+              ),
+              const SizedBox(
+                width: 27,
+              ),
+              Text(
+                "Settings ",
+                style: TextStyle(color: white.withOpacity(0.7), fontSize: 18),
+              )
+            ],
+          ),
+        )),
+  );
 }
+
